641da960c09445ec1babc049a606cf2ccbfa7ee3
VadimsAhmers <vadimspring@mail.ru>
2018-02-07 13:24:32 +0500

    Все доделал

--- src/DBTester.java
+++ src/DBTester.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 public class DBTester {
@@ -165,8 +171,163 @@ public class DBTester {
         }
     }
 
-    void createTablesIfNeeded(){
-        
+    void createTablesIfNeeded() {
+        ResultSet databaseTables = null;
+        boolean table1Exists = false, table2Exists = false;
+
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            databaseTables =
+                    metaData.getTables(null, null, "%", null);
+            while (databaseTables.next()) {
+                if (databaseTables.getString(3).equals("ITEM")) table1Exists = true;
+                if (databaseTables.getString(3).equals("ITEMGROUP")) table2Exists = true;
+            }
+            if (!table1Exists & !table2Exists) createAndFillTablesFromFile();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (databaseTables != null) databaseTables.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        void createAndFillTablesFromFile(){
+
+        try(Statement statement = connection.createStatement(); BufferedReader br = new BufferedReader(new InputStreamReader
+                (new FileInputStream("createTables.sql")))){
+
+                String s1, s2="";
+                while ((s1=br.readLine())!=null) {
+                    s2 += s1;
+                    if (s1.endsWith(";")) {
+                        statement.executeQuery(s2);
+                        s2 = "";
+                    }
+                }
+            System.out.println("Таблицы созданы и заполнены");
+            }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    boolean addItemToGroup(String itemName, String groupName){
+        try(PreparedStatement statement =
+                    connection.prepareStatement("INSERT INTO ITEM(TITLE, GROUPID) VALUES (?,?)")){
+            statement.setString(1, itemName);
+            statement.setInt(2, getGroupID(groupName));
+            statement.execute();
+            System.out.printf("Студент %s добавлен(а)\n", itemName);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    boolean removeItemFromGroup(String itemName, String groupName){
+        try(PreparedStatement statement =
+                    connection.prepareStatement
+                            ("DELETE FROM ITEM WHERE TITLE=? AND GROUPID=?")){
+            statement.setString(1, itemName);
+            statement.setInt(2, getGroupID(groupName));
+            statement.execute();
+            System.out.printf("Студент %s удален(а)\n", itemName);
+            return true;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    void editTablseFromFile(){
+        boolean everythingIsOk = false;
+        try(Statement statement = connection.createStatement();
+            BufferedReader br = new BufferedReader(new InputStreamReader
+                (new FileInputStream("items.txt")))){
+
+            connection.setAutoCommit(false);
+            String s;
+
+            while ((s=br.readLine())!=null) {
+                if (s.contains("++")) {
+                    String[] str = s.split("\\++");
+                    everythingIsOk = addItemToGroup(str[1], str[0]);
+                }else
+                    if (s.contains("--")) {
+                    String[] str = s.split("--");
+                    everythingIsOk = removeItemFromGroup(str[1], str[0]);
+                }
+                if (!everythingIsOk){
+                    connection.rollback();
+                    break;
+                }
+            }
+            if (everythingIsOk) connection.commit();
+
+        }catch (SQLException|IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void editGroupsFromFile(){
+        Set<String> groupsToAdd = new HashSet<>();
+        Set<String> groupsToRemove = new HashSet<>();
+        int lastGroupID=0;
+
+        try(Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            BufferedReader br = new BufferedReader(new InputStreamReader
+                    (new FileInputStream("groups.txt")));
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ITEMGROUP")){
+
+            connection.setAutoCommit(false);
+            String s;
+
+            while ((s=br.readLine())!=null) {
+                if (s.contains("++")) {
+                    groupsToAdd.add(s.replace("++", ""));
+                }else
+                if (s.contains("--")) {
+                    String toDelete = s.replace("--", "");
+                    if (groupsToAdd.contains(toDelete)) groupsToAdd.remove(toDelete);
+                    else groupsToRemove.add(toDelete);
+                }
+            }
+
+            while (resultSet.next()){
+                String groupName = resultSet.getString(2);
+                if (groupsToRemove.contains(groupName)) resultSet.deleteRow();
+                if (groupsToAdd.contains(groupName)) groupsToAdd.remove(groupName);
+                lastGroupID++;
+            }
+
+            for (String str : groupsToAdd) {
+                    resultSet.moveToInsertRow();
+                    resultSet.updateInt(1, lastGroupID++);
+                    resultSet.updateString(2, str);
+                    resultSet.insertRow();
+                }
+            connection.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         try {
             DBTester tester = new DBTester();
             tester.connectToDB();
-            tester.test();
+            //tester.createTablesIfNeeded();
+            //tester.test();
+            System.out.println(tester.addItemToGroup("Иванова И.И.", "РБП-107м"));
+            System.out.println(tester.removeItemFromGroup("Иванова И.И.", "РБП-107м"));
             //tester.viewGroups();
             //tester.viewItems();
             //tester.printContent();
-            //tester.viewItemsInGroup(2);
+            //tester.viewItemsInGroup(1);
+            tester.createTablesIfNeeded();
+            //tester.editTablseFromFile();
+            tester.editGroupsFromFile();
             tester.connection.close();
 
         } catch (SQLException e) {
